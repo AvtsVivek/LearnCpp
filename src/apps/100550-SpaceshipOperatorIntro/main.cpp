2da7fff9ec10d635d33708c5875568da2d0b9f5d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-11-29 09:53:01 +0530

    Added more info

--- src/apps/100550-SpaceshipOperatorIntro/main.cpp
+++ src/apps/100550-SpaceshipOperatorIntro/main.cpp
@@ -1,9 +1,16 @@
 
 
-# include <iostream>
+#include <iostream>
+
+/**
+ * @brief And example program.
+ * @return REturns 0 when execution completes successifullyl
+ */
 
 int main()
 {
     auto result = (10 <=> 20) > 0;
     std::cout << result << std::endl;
+    std::cout << "Here we go...and go...";
+    std::cout.flush();
 }
---
+++ src/tasks/100550-SpaceshipOperatorIntro/commands.sh
@@ -0,0 +1,11 @@
+
+
+cd ../../..
+
+cd src/tasks/100550-SpaceshipOperatorIntro
+
+cd src/apps/100550-SpaceshipOperatorIntro
+
+code .
+
+code . -r
