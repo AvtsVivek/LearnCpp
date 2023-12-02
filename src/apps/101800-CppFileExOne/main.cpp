050d523ec207172a263628d365bb6bbffc362471
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-12-02 17:35:39 +0530

    Added more info

---
+++ src/apps/101800-CppFileExOne/main.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+/**
+ * @brief And example program.
+ * @return REturns 0 when execution completes successifullyl
+ */
+
+int main()
+{
+    auto result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
+    std::cout << "Hellow....";
+}
---
+++ src/tasks/101800-CppFileExOne/commands.sh
@@ -0,0 +1,14 @@
+
+cd ../../..
+
+cd src/tasks/101800-CppFileExOne
+
+cd src/apps/101800-CppFileExOne
+
+code .
+
+start .
+
+code . -r
+
+
