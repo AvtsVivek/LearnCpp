13c368a32dc876e1c4c3c0307e5917295d434158
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-12-04 12:46:27 +0530

    Added more info

--- src/apps/102900-InputOutput/main.cpp
+++ src/apps/102900-InputOutput/main.cpp
@@ -2,8 +2,11 @@
 #include <string>
 
 int main(){
+
    //Printing data
+  
    /*
+
    std::cout << "Hello C++20 " << std::endl;
 
    int age{21};
@@ -11,12 +14,11 @@ int main(){
 
    std::cerr << "Error message : Something is wrong" << std::endl;
    std::clog << "Log message : Something happened " << std::endl; 
-
-   */
+  
+    */
 
    //Data input
 
-   /*
    int age1;
    std::string name;
 
@@ -25,11 +27,13 @@ int main(){
    //std::cin >> name;
    //std::cin >> age1;
 
+   // Chaining the input
+
    std::cin >> name >> age1;
 
    std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
 
-    */
+   
 
     //Data with spaces
 
@@ -38,14 +42,14 @@ int main(){
 
     std::cout << "Please type in your full name and age " << std::endl;
 
+    std::cin.ignore();
+
     std::getline(std::cin,full_name);
 
     std::cin >> age3;
 
     std::cout << "Hello " << full_name 
-            << " you are " << age3 << " years old!" << std::endl;
-
-
+            << " you are " << age3 << " years old!!!!" << std::endl;
  
   return 0;
 }
