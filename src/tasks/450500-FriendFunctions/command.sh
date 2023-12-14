7785f2cae8a4c5cbed1ab7bedc7c3065105687a5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-12-14 13:43:54 +0530

    Added new projects

---
+++ src/tasks/450500-FriendFunctions/command.sh
@@ -0,0 +1,14 @@
+
+cd ../../..
+
+cd src/tasks/450500-FriendFunctions
+
+cd src/apps/450500-FriendFunctions
+
+start .
+
+code .
+
+code . -r
+
+
---
+++ src/tasks/450550-FriendClasses/command.sh
@@ -0,0 +1,14 @@
+
+cd ../../..
+
+cd src/tasks/450550-FriendClasses
+
+cd src/apps/450550-FriendClasses
+
+start .
+
+code .
+
+code . -r
+
+
