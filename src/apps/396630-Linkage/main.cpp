9f5e251400dec58306e04037db6dde971354450d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-01-06 14:22:46 +0530

    Added more info

--- src/apps/396630-Linkage/main.cpp
+++ src/apps/396630-Linkage/main.cpp
@@ -12,27 +12,28 @@ int item_count {6}; // External linkage
 
 
 
-extern void print_distance();
+void print_distance();
 extern void print_item_count();
 
 int main(){
 
-    /*
+    
     std::cout << "distance(main) : " << distance << "  &distance : " << &distance <<std::endl;
 
     std::cout << "-----" << std::endl;
 
     print_distance();
 
-    */
+    
 
+    /*
     std::cout << "item_count(main) : " << item_count << " &item_count : "
         << &item_count << std::endl;
 
     std::cout << "-----" << std::endl;
     print_item_count();
 
-   
+    */ 
     
     return 0;
 }
--- src/tasks/396580-OneDefinitionRule/ReadMe.md
+++ src/tasks/396580-OneDefinitionRule/ReadMe.md
@@ -1,10 +1,10 @@
 # Auto
 
 ## Notes
-1. 
+1. Defs cant show up more than once in entire program, or translation unit.
 
 
 ## References
 
-1. 
+1. https://learn.microsoft.com/en-us/cpp/cpp/program-and-linkage-cpp
 
