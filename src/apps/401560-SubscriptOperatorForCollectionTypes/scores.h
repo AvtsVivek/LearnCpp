d5b9129ef901559f42c87ef735ad4fd5f205c1d2
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-01-21 07:07:21 +0530

    Added more info

--- src/apps/401560-SubscriptOperatorForCollectionTypes/scores.h
+++ src/apps/401560-SubscriptOperatorForCollectionTypes/scores.h
@@ -4,14 +4,14 @@
 #include <string>
 #include <iostream>
 
-
-
-class Scores{
-    public : 
-        Scores() = delete;
-        Scores(const std::string& course_name)
-            : m_course_name{course_name}
-        {}
+class Scores
+{
+public:
+    Scores() = delete; // deleted because its not usful.
+    Scores(const std::string &course_name)
+        : m_course_name{course_name}
+    {
+    }
 
     /*
     double& operator[](size_t index){
@@ -24,21 +24,21 @@ class Scores{
         return m_scores[index];
     }
     */
-
-    double& operator[](size_t index);
+    double &operator[](size_t index);
     double operator[](size_t index) const;
 
-
-    void print_info()const{
+    void print_info() const
+    {
         std::cout << m_course_name << " : [ ";
-        for(size_t i{}; i < 20; ++i){
+        for (size_t i{}; i < 20; ++i)
+        {
             std::cout << m_scores[i] << " ";
         }
         std::cout << "]" << std::endl;
     }
 
-    private: 
-        std::string m_course_name;
-        double m_scores[20]{};
+private:
+    std::string m_course_name;
+    double m_scores[20]{};
 };
 #endif // SCORES
--- src/tasks/401560-SubscriptOperatorForCollectionTypes/ReadMe.md
+++ src/tasks/401560-SubscriptOperatorForCollectionTypes/ReadMe.md
@@ -1,7 +1,19 @@
 # Auto
 
 ## Notes
-1. 
+
+1. We are going to have the ability to modify the data using a subscript operator.
+
+2. We also have the ability to read the data using another subscript operator.
+
+```cpp
+double &operator[](size_t index);
+double operator[](size_t index) const;
+```
+
+3. Const and non const operators. 
+
+4. 
 
 
 ## References
