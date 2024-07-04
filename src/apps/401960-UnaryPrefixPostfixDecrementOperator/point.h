5a060e55eb19a6941d039750491e5efd3dd45b1a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-07-04 09:34:04 +0530

    Added more info

--- src/apps/401960-UnaryPrefixPostfixDecrementOperator/point.h
+++ src/apps/401960-UnaryPrefixPostfixDecrementOperator/point.h
@@ -4,35 +4,34 @@
 
 class Number;
 
-
 class Point
 {
-	friend std::ostream& operator<<(std::ostream& os, const Point& p);
-	friend void operator++(Point& operand);
-	friend void operator--(Point& operand);
-	
+	friend std::ostream &operator<<(std::ostream &os, const Point &p);
+	friend void operator++(Point &operand);
+	friend void operator--(Point &operand);
+
 public:
 	Point() = default;
-	Point(double x, double y) : 
-		m_x(x), m_y(y){
+	Point(double x, double y) : m_x(x), m_y(y)
+	{
 	}
 	~Point() = default;
 
-private: 
-	double length() const;   // Function to calculate distance from the point(0,0)
+private:
+	double length() const; // Function to calculate distance from the point(0,0)
 
-private : 
-	double m_x{}; 
-	double m_y{}; 
+private:
+	double m_x{};
+	double m_y{};
 };
-Point operator++(Point& operand,int);
-Point operator--(Point& operand,int);
 
+Point operator++(Point &operand, int);
+Point operator--(Point &operand, int);
 
-inline std::ostream& operator<<(std::ostream& os, const Point& p){
-	os << "Point [ x : " << p.m_x << ", y : " << p.m_y << "]";	
+inline std::ostream &operator<<(std::ostream &os, const Point &p)
+{
+	os << "Point [ x : " << p.m_x << ", y : " << p.m_y << "]";
 	return os;
 }
-
 
 #endif // POINT_H
--- src/tasks/401960-UnaryPrefixPostfixDecrementOperator/ReadMe.md
+++ src/tasks/401960-UnaryPrefixPostfixDecrementOperator/ReadMe.md
@@ -1,7 +1,7 @@
 # Auto
 
 ## Notes
-1. 
+1. This is similar to the earlier examples. 
 
 
 ## References
