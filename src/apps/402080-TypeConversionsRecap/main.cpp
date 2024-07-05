8078c2a0065351125cc6a75ffcf34efa59c1ae17
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-07-05 12:10:10 +0530

    Added more info

--- src/apps/402080-TypeConversionsRecap/main.cpp
+++ src/apps/402080-TypeConversionsRecap/main.cpp
@@ -1,15 +1,15 @@
- #include <iostream>
+#include <iostream>
 #include "point.h"
 #include "number.h"
 
-void do_something_with_point(const Point& p){
+void do_something_with_point(const Point &p)
+{
     std::cout << "point : " << p << std::endl;
 }
 
-
 int main(int argc, char **argv)
 {
-	Point p1(10,10);
+    Point p1(10, 10);
     Number n1(22);
 
     p1 = n1;
@@ -17,11 +17,9 @@ int main(int argc, char **argv)
     // .(2) Constructor taking in a Number
     // .(3) Type conversion operator : Number -> Point
 
-
-
     do_something_with_point(n1);
     // .(1) Constructor
     // .(2) Type conversion operator
-	
-	return 0;
+
+    return 0;
 }
--- src/tasks/402080-TypeConversionsRecap/ReadMe.md
+++ src/tasks/402080-TypeConversionsRecap/ReadMe.md
@@ -2,6 +2,9 @@
 
 ## Notes
 1. HEre we have various options for conversions. The priority that the compiler choose is demoed here.
+   1. Copy assignment operator
+   2. Constructor taking in a Number
+   3. Type conversion operator : Number -> Point
 
 ## References
 
