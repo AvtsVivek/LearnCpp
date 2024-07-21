cdc0d22553169e10b4e4fc95a34c1fea7df5f798
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-07-21 10:22:07 +0530

    Added more info

--- src/apps/406820-InheritingBaseConstructors/main.cpp
+++ src/apps/406820-InheritingBaseConstructors/main.cpp
@@ -2,10 +2,11 @@
 #include "person.h"
 #include "engineer.h"
 
-int main(){
+int main()
+{
 
-	Engineer eng1("Daniel Gray",23,"asdl;fkjdas;fkdas;fk",4);
+	Engineer eng1("Daniel Gray", 23, "asdl;fkjdas;fkdas;fk", 4);
 	std::cout << "eng1 : " << eng1 << std::endl;
 
-    return 0;
+	return 0;
 }
--- src/tasks/406820-InheritingBaseConstructors/ReadMe.md
+++ src/tasks/406820-InheritingBaseConstructors/ReadMe.md
@@ -3,8 +3,10 @@
 ## Notes
 1. 
 
+![Copy Ctro](50_50_Copy_Ctor.jpg)
 
 ## References
 
 1. 
+
 
