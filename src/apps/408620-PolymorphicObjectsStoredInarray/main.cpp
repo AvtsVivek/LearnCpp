9e65dc05a6a847c0a221de5e9c0c4e0135555ff6
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-07-31 13:53:56 +0530

    Added more info

---
+++ src/apps/408620-PolymorphicObjectsStoredInarray/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <memory>
+#include "shape.h"
+#include "oval.h"
+#include "circle.h"
+
+int main()
+{
+
+    Circle circle1(7.2, "circle1");
+    Oval oval1(13.3, 1.2, "Oval1");
+    Circle circle2(11.2, "circle2");
+    Oval oval2(31.3, 15.2, "Oval2");
+    Circle circle3(12.2, "circle3");
+    Oval oval3(53.3, 9.2, "Oval3");
+
+    // If you store raw object data in an array set up to store base class objects
+    // the data is going to be sliced off!
+
+    std::cout << "sizeof (circle1) : " << sizeof(circle1) << std::endl;
+    Shape shapes1[]{circle1, oval1, circle2, oval2, circle3, oval3};
+
+    for (Shape &s : shapes1)
+    {
+        std::cout << "sizeof (object) : " << sizeof(s) << ", s.draw(): ";
+        s.draw();
+    }
+
+    return 0;
+}
---
+++ src/tasks/408620-PolymorphicObjectsStoredInarray/command.sh
@@ -0,0 +1,25 @@
+
+cd ../../..
+
+cd src/tasks/408620-PolymorphicObjectsStoredInarray
+
+cd src/apps/408620-PolymorphicObjectsStoredInarray
+
+start .
+
+code .
+
+code . -r
+
+dir
+
+g++ --version
+
+# -o flag means, compile as well as link.
+g++ "-static" -o main.exe .\*.cpp
+
+dir
+
+.\main.exe
+
+
