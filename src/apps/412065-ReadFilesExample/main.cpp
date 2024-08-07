2393ea18fee473af968e31963c44338ba531118e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-08-07 12:45:59 +0530

    Added more info

--- src/apps/412065-ReadFilesExample/main.cpp
+++ src/apps/412065-ReadFilesExample/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <exception>
-#include <string_view>
-using namespace std;
+#include <sstream>
 
-void read_from_file(std::string_view filename, size_t size, int *data)
+void read_from_file(std::string_view filename, size_t size, int * data)
 {
 	std::ifstream fstrem_in{filename.data()}; // We may fail to open the file
 
@@ -13,42 +13,42 @@ void read_from_file(std::string_view filename, size_t size, int *data)
 	{
 
 		// We failed to open the file: throw an exception.
-		std::string fail_info = "Failed to open the file: " + static_cast<std::string>(filename);
-		throw std::exception(fail_info);
-	}
+		std::stringstream ss;
+        ss << "Unable to open file " << filename ;
+        throw std::runtime_error{ ss.str().c_str() };
+    }
 
-	int value;
-	int index{};
+    
+    int value;
+    int index{};
+    while (fstrem_in >> value) {
+        data[index++] = value;
+    }
+    //Something might go wrong in the process of reading the file.
+    if (!fstrem_in.eof()) {
+        throw std::runtime_error{ "Couldn't read the file." };
+    }
 
-	while (fstrem_in >> value)
-	{
-		data[index++] = value;
-	}
-
-	// Something might go wrong in the process of reading the file.
 }
 
 int main()
 {
 
-	int data[4]{};
 
-	try
-	{
-		read_from_file("numberss.txt", std::size(data), data);
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << '\n';
-		std::cout << "END with exception." << std::endl;
-		return 1;
-	}
+    int data[4]{};
+    try
+    {
+        read_from_file("numbers.txt",std::size(data),data);
+        for(auto i : data){
+                std::cout << "value : " << i << std::endl;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 
-	for (auto i : data)
-	{
-		std::cout << "value : " << i << std::endl;
-	}
+    std::cout << "END" << std::endl;
 
-	std::cout << "END." << std::endl;
-	return 0;
+    return 0;
 }
