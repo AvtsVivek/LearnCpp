f36a7a2bf62e7b82dbf64b81db28f3156a515d9d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-08-09 11:49:20 +0530

    Added more info

--- src/apps/414620-OtherOperators/stream_insertable.h
+++ src/apps/414620-OtherOperators/stream_insertable.h
@@ -2,11 +2,12 @@
 #define STREAM_INSERTABLE_H
 #include <iostream>
 
-class StreamInsertable{
-    friend std::ostream& operator<< (std::ostream& out, const StreamInsertable& operand);
-    
-public : 
-    virtual void stream_insert(std::ostream& out)const =0;
+class StreamInsertable
+{
+    friend std::ostream &operator<<(std::ostream &out, const StreamInsertable &operand);
+
+public:
+    virtual void stream_insert(std::ostream &out) const = 0;
 };
 
-#endif //STREAM_INSERTABLE_H
+#endif // STREAM_INSERTABLE_H
--- src/tasks/414620-OtherOperators/ReadMe.md
+++ src/tasks/414620-OtherOperators/ReadMe.md
@@ -1,10 +1,10 @@
 # Auto
 
 ## Notes
-1. 
-
+1. Compound addition operator `operator+=`
+2. Addition operator `operator+`
+3. Copy assignment operator `operator=`.
 
 ## References
-
 1. 
 
