daadbd832927b57722afd447bb0efb04b3b46435
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-08-18 16:40:33 +0530

    Added more info

--- src/apps/416744-StreamInsertionOperatorForTemplateWithParams/main.cpp
+++ src/apps/416744-StreamInsertionOperatorForTemplateWithParams/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include "boxcontainer.h"
+#include "point.h"
 
+int main()
+{
+    Point<int,3> point_3d;
+    point_3d.set_value(0,10);
+    point_3d.set_value(1,20);
+    point_3d.set_value(2,30);
 
+    std::cout << point_3d << std::endl;
 
-int main(){
-
-    BoxContainer<int> int_box;
-    int_box.add(1);
-    int_box.add(2);
-    int_box.add(10);
-    int_box.add(15);
-
-    std::cout << "int_box : " << int_box << std::endl;
-   
     return 0;
 }
---
+++ src/tasks/416746-OutputStreamInsertionOperator/command.sh
@@ -0,0 +1,24 @@
+
+cd ../../..
+
+cd src/tasks/416744-StreamInsertionOperatorForTemplateWithParams
+
+cd src/apps/416744-StreamInsertionOperatorForTemplateWithParams
+
+start .
+
+code .
+
+code . -r
+
+dir
+
+g++ --version
+
+# -o flag means, compile as well as link.
+g++ "-static" -o main.exe .\*.cpp
+
+dir
+
+.\main.exe
+
