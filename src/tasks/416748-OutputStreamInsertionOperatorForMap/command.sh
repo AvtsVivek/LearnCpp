c77d27291d81e0cd32bf2ffc1708c965cdf81deb
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-08-26 13:52:57 +0530

    Added more info

--- src/tasks/416748-OutputStreamInsertionOperatorForMap/command.sh
+++ src/tasks/416748-OutputStreamInsertionOperatorForMap/command.sh
@@ -18,6 +18,9 @@ g++ --version
 # -o flag means, compile as well as link.
 g++ "-static" -o main.exe .\*.cpp
 
+# -o flag means, compile as well as link.
+g++ "-static" -o main.exe .\*.cpp -std=c++20
+
 dir
 
 .\main.exe
