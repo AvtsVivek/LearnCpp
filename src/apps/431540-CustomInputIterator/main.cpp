7faa1dba786ae92973bc6a8e1903134ec459b5e6
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-09-21 17:44:47 +0530

    Added more info

--- src/apps/431540-CustomInputIterator/main.cpp
+++ src/apps/431540-CustomInputIterator/main.cpp
@@ -4,19 +4,21 @@
 #include "boxcontainer.h"
 
 template <typename T>
-std::ostream& operator<<( std::ostream& out,const  std::vector<T>& vec){
+std::ostream &operator<<(std::ostream &out, const std::vector<T> &vec)
+{
     out << " [ ";
-    for(auto i : vec){
-        out << i  << " ";
+    for (auto i : vec)
+    {
+        out << i << " ";
     }
     out << "]";
     return out;
 }
 
+int main()
+{
 
-int main(){
-
-    //std::vector<int> box1 {8,1,4,2,5,3,7,9};
+    // std::vector<int> box1 {8,1,4,2,5,3,7,9};
     BoxContainer<int> box1;
     box1.add(5);
     box1.add(1);
@@ -27,28 +29,28 @@ int main(){
     box1.add(7);
     box1.add(9);
     box1.add(6);
-   
 
     std::cout << "box : " << box1 << std::endl;
 
-    //find algorithm
-    if (std::ranges::find(box1, 8) != box1.end()) {
+    // find algorithm
+    if (std::ranges::find(box1, 8) != box1.end())
+    {
         std::cout << "numbers contains: " << 8 << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "numbers does not contain: " << 8 << std::endl;
     }
 
+    // std::ranges::reverse(box1.begin(),box1.end());
 
-    //std::ranges::reverse(box1.begin(),box1.end());
-
-
-    //Range based for loop
-    /*
+    // Range based for loop
+    
     for(auto n : box1){
         std::cout << n << " ";
     }
     std::cout << std::endl;
-    */
-   
+    
+
     return 0;
 }
--- src/tasks/431540-CustomInputIterator/ReadMe.md
+++ src/tasks/431540-CustomInputIterator/ReadMe.md
@@ -1,7 +1,15 @@
 # Auto
 
 ## Notes
-1. 
+1. Custom iterator Requirements
+   1. Your container class needs to be a class template.
+   2. The container class has to model iterator types.
+   3. Your container needs begin() and end() methods thatreturn those iterators.
+   4. The iterators have to model the operators needed by your algorithms.
+2. Needs to provide the type aliases expected by the standard template library.
+3. These types alias help alogrithms work better. 
+4. Modify the box container and make it support input iterators.
+5. 
 
 
 ## References
